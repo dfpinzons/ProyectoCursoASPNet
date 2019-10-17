6c983060fa8f967bcc3975d9c3e47b2c888e39e6
David Pinzon <dfpinzons@unal.edu.co>
2019-10-17 18:17:38 -0500

    Primer commit del proyecto

---
+++ WebApplication1/Controllers/EscuelaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Views.Escuela
+{
+    public class EscuelaController : Controller
+    {
+        // Tipo generico
+        public IActionResult Index()
+        {
+            // El view es el Index.cshtml de View/Escuela
+            return View();
+        }
+
+    }
+}
